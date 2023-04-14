3af5279349db0ebde113ec94008d6af587b5484e
Matteo Cusin <matteo.cusin58@gmail.com>
2023-04-14 15:35:35 +0200

    SL-302 Aggiunti test metodi di scrittura

--- CoreTests/DateOnlyJsonConverterTests.cs
+++ CoreTests/DateOnlyJsonConverterTests.cs
@@ -14,14 +14,13 @@ namespace Core.Tests
         }
 
         [TestMethod()]
-        public void Read_ReturnsParsedDateOnly()
-        {
+        public void Read_ReturnsParsedDateOnly() {
             // Arrange
             Utf8JsonReader reader = new Utf8JsonReader(Encoding.UTF8.GetBytes("{ \"date\" : \"21/05/2001\" }"));
             JsonSerializerOptions options = new JsonSerializerOptions();
             Type typeToConvert = typeof(DateOnly);
 
-            var converter = new DateOnlyJsonConverter();
+            DateOnlyJsonConverter converter = new DateOnlyJsonConverter();
 
             // Act
             DateOnly result = converter.Read(ref reader, typeToConvert, options);
@@ -32,17 +31,40 @@ namespace Core.Tests
         
         [TestMethod()]
         [ExpectedException(typeof(FormatException))]
-        public void Read_WrongFormatDate()
-        {
+        public void Read_WrongFormatDate() {
             // Arrange
             Utf8JsonReader reader = new Utf8JsonReader(Encoding.UTF8.GetBytes("{  \"date\" : \"21/15/2001\"  }"));
             JsonSerializerOptions options = new JsonSerializerOptions();
             Type typeToConvert = typeof(DateOnly);
 
-            var converter = new DateOnlyJsonConverter();
+            DateOnlyJsonConverter converter = new DateOnlyJsonConverter();
 
             // Act
             converter.Read(ref reader, typeToConvert, options);
+        }
+        
+        [TestMethod()]
+        public void Write_Correct() {
+            
+            // Arrange
+            MemoryStream stream = new MemoryStream();
+            Utf8JsonWriter writer = new Utf8JsonWriter(stream);
+            JsonSerializerOptions options = new JsonSerializerOptions();
+            
+            DateOnly value = new DateOnly(2001,05,21);
+            string serializationFormat = "dd/MM/yyyy";
+            DateOnlyJsonConverter converter = new DateOnlyJsonConverter(serializationFormat);
+
+            // Act
+            converter.Write(writer, value, options);
+            
+            // Pre - Assertion
+            writer.Flush();
+            byte[] result = stream.ToArray();
+            string jsonString = Encoding.UTF8.GetString(result);
+            
+            // Assert
+            Assert.AreEqual($"\"{value.ToString(serializationFormat)}\"", jsonString);
         }
     }
 }
--- CoreTests/TimeOnlyJsonConverterTests.cs
+++ CoreTests/TimeOnlyJsonConverterTests.cs
@@ -22,7 +22,7 @@ namespace Core.Tests {
             JsonSerializerOptions options = new JsonSerializerOptions();
             Type typeToConvert = typeof(TimeOnly);
 
-            var converter = new TimeOnlyJsonConverter();
+            TimeOnlyJsonConverter converter = new TimeOnlyJsonConverter();
 
             // Act
             TimeOnly result = converter.Read(ref reader, typeToConvert, options);
@@ -33,17 +33,41 @@ namespace Core.Tests {
         
         [TestMethod()]
         [ExpectedException(typeof(FormatException))]
-        public void Read_WrongFormatDate()
+        public void Read_WrongFormatTime()
         {
             // Arrange
             Utf8JsonReader reader = new Utf8JsonReader(Encoding.UTF8.GetBytes("{  \"time\" : \"13:30:00:000\"  }"));
             JsonSerializerOptions options = new JsonSerializerOptions();
             Type typeToConvert = typeof(TimeOnly);
 
-            var converter = new TimeOnlyJsonConverter();
+            TimeOnlyJsonConverter converter = new TimeOnlyJsonConverter();
 
             // Act
             converter.Read(ref reader, typeToConvert, options);
+        }
+
+        [TestMethod()]
+        public void Write_Correct() {
+            
+            // Arrange
+            MemoryStream stream = new MemoryStream();
+            Utf8JsonWriter writer = new Utf8JsonWriter(stream);
+            JsonSerializerOptions options = new JsonSerializerOptions();
+            
+            TimeOnly value = new TimeOnly(10, 30);
+            string serializationFormat = "hh:mm";
+            TimeOnlyJsonConverter converter = new TimeOnlyJsonConverter(serializationFormat);
+
+            // Act
+            converter.Write(writer, value, options);
+            
+            // Pre - Assertion
+            writer.Flush();
+            byte[] result = stream.ToArray();
+            string jsonString = Encoding.UTF8.GetString(result);
+            
+            // Assert
+            Assert.AreEqual($"\"{value.ToString(serializationFormat)}\"", jsonString);
         }
 
     }
