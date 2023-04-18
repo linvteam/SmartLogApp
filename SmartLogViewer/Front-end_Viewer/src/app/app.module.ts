b9f51a545edea86ff919f270516804558c970ef1
Riccardo014 <116018411+Riccardo014@users.noreply.github.com>
2023-04-18 16:18:47 +0200

    SL-323 aggiunti component

--- SmartLogViewer/Front-end_Viewer/src/app/app.module.ts
+++ SmartLogViewer/Front-end_Viewer/src/app/app.module.ts
@@ -4,11 +4,21 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { FileUploadComponent } from './components/file-upload/file-upload.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { TableComponent } from './table/table.component';
+import { ChartComponent } from './chart/chart.component';
+import { FileInfoComponent } from './file-info/file-info.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FileUploadComponent
+    FileUploadComponent,
+    NavBarComponent,
+    TableComponent,
+    ChartComponent,
+    FileInfoComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
---
+++ SmartLogViewer/Front-end_Viewer/src/app/file-info/file-info.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-file-info',
+  templateUrl: './file-info.component.html',
+  styleUrls: ['./file-info.component.css']
+})
+export class FileInfoComponent {
+
+}
