0c1e3dd893bbc886dccbbd896141260f4e0a9ede
Riccardo014 <116018411+Riccardo014@users.noreply.github.com>
2023-04-18 15:57:35 +0200

    SL-323 fix stile

--- SmartLogViewer/Front-end_Viewer/src/app/app.component.ts
+++ SmartLogViewer/Front-end_Viewer/src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular 15 File Upload example';
+  title = 'Carica un file';
 }
 
