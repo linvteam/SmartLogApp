85e92628495969685b6f0837f795640d7cf17aeb
Matteo Cusin <matteo.cusin58@gmail.com>
2023-04-27 11:07:24 +0200

    SL-335 Prima versione file info

--- SmartLogViewer/Frontend/src/app/components/file-info/file-info.component.ts
+++ SmartLogViewer/Frontend/src/app/components/file-info/file-info.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import {ColDef} from 'ag-grid-community';
+import { LogService } from 'src/app/services/log.service';
+import {INIFile, Log} from "../../log.classes";
+import localeIT from "@angular/common/locales/it"
+import {formatDate, registerLocaleData} from "@angular/common";
+registerLocaleData(localeIT, "it");
 
 @Component({
   selector: 'app-file-info',
@@ -7,4 +13,34 @@ import { Component } from '@angular/core';
 })
 export class FileInfoComponent {
 
+  pcDate: string;
+  upsDate: string;
+  iniFiles: INIFile[];
+  columnDefs: ColDef[];
+  defaultColDef: ColDef;
+  
+
+  constructor(private logService: LogService) {
+    
+    const format = 'dd/MM/yyyy - hh:mm:ss';
+    let pcDateToConvert = this.logService.getLog().Header.PCDate;
+    let upsDateToConvert = this.logService.getLog().Header.UPSDate;
+    const locale = 'it-IT';
+    
+    this.pcDate = formatDate(pcDateToConvert, format, locale);
+    this.upsDate = formatDate(upsDateToConvert, format, locale);;
+    
+    this.columnDefs = [
+      { field: 'fileName' },
+      { field: 'unit' },
+      { field: 'subUnit' },
+    ];
+    
+    this.defaultColDef ={
+      sortable: true, filter: true, resizable: true, suppressSizeToFit: true
+    };
+    
+    this.iniFiles = this.logService.getLog().Header.INIFile;
+  }
+  
 }
