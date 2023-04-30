e399913132956426cdcea4cb2d171c3afdacc86c
Nicola Ravagnan <nicolaravagnan4@gmail.com>
2023-04-30 13:25:39 +0200

    SL-327 rimosso codice spazzatura

--- SmartLogViewer/Frontend/src/app/services/log.service.ts
+++ SmartLogViewer/Frontend/src/app/services/log.service.ts
@@ -9,31 +9,18 @@ export class LogService {
 
     constructor() {
         this.actualLog = undefined;
-        this.displayLog = undefined;
     } 
 
-
     private actualLog?: Log;
-    private displayLog?: Log;
 
     public set Log (log: Log) {
         this.actualLog = log;
-        this.displayLog = new Log(this.actualLog as Log)
     }
 
     public getLog() : Log {
         return (this.actualLog) as Log;
     }
 
-    public getDisplayLog(): Log {
-        return (this.displayLog) as Log;
-    }
-
-    public resetDisplayLog(): void {
-        if (this.validLog()) {
-            this.displayLog = new Log(this.actualLog as Log)
-        }
-    }
 
     public validLog(): boolean {
         return this.actualLog != undefined;
@@ -41,7 +28,6 @@ export class LogService {
 
     public clean(): void {
         this.actualLog = undefined;
-        this.displayLog = undefined;
     }
 
 }
