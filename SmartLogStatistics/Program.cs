8d5a3bb3e6b1aaee02df39104edec3c333ac9853
Casazio <albertocasadomoreno127@gmail.com>
2023-05-19 16:12:24 +0200

    aggiunto UploadController

--- SmartLogStatistics/Program.cs
+++ SmartLogStatistics/Program.cs
@@ -1,10 +1,25 @@
+using Core;
 using Microsoft.EntityFrameworkCore;
 using Npgsql;
 using SmartLogStatistics.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSingleton<Parser>();
 
 // Add services to the container.
+
+builder.Services.AddControllers().AddJsonOptions(options => {
+    options.JsonSerializerOptions.Converters.Add(new DateOnlyJsonConverter());
+    options.JsonSerializerOptions.Converters.Add(new TimeOnlyJsonConverter());
+});
+
+builder.Services.AddCors(options => options.AddPolicy(name: "FrontendUI",
+    policy => {
+        policy.WithOrigins("http://localhost:4200").AllowAnyMethod().AllowAnyHeader();
+    }
+));
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -17,18 +32,19 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    app.UseHsts();
-} 
-else
+if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
     app.UseMigrationsEndPoint();
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseCors("FrontendUI");
 }
+else
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+} 
 
 using (var scope = app.Services.CreateScope())
 {
