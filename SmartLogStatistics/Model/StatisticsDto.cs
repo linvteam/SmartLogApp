466ef4cc579d2323e4a49b3cf1505156b26702f2
Alessandro <alebaldissera99@gmail.com>
2023-05-19 23:02:16 +0200

    SL-377 stesura repository statistics

--- SmartLogStatistics/Model/StatisticsDto.cs
+++ SmartLogStatistics/Model/StatisticsDto.cs
@@ -1,4 +1,27 @@
-﻿namespace SmartLogStatistics.Model {
+﻿using NuGet.Protocol.Plugins;
+using System.Security.Policy;
+
+namespace SmartLogStatistics.Model {
     public class StatisticsDto {
+
+        public class Statistic {
+            public string Name { get; private set; }
+            public double Value { get; private set; }
+
+            public Statistic(string name, double value) {
+                Name = name;
+                Value = value;
+            }
+        }
+
+        public DateTime StartDate { get; private set; }
+        public DateTime EndDate { get; private set; }
+        public List<Statistic> Statistics { get; private set; }
+
+        public StatisticsDto(DateTime startDate, DateTime endDate, List<Statistic> statistics) {
+            StartDate = startDate;
+            EndDate = endDate;
+            Statistics = statistics;
+        }
     }
 }
---
+++ SmartLogStatistics/Repository/StatisticsRepositoryPgSql.cs
@@ -0,0 +1,20 @@
+﻿using SmartLogStatistics.Model;
+
+namespace SmartLogStatistics.Repository {
+    public class StatisticsRepositoryPgSql: StatisticsRepository {
+
+        private readonly SmartLogContext context;
+
+        public StatisticsRepositoryPgSql(SmartLogContext db) {
+            context = db;
+        }
+
+        public StatisticsDto Statistics(DateTime start, DateTime end) {
+
+            var res = from l in context.Log from f in context.File.Where(x => x.id == l.file_id) select new { f.id };
+            
+
+            throw new NotImplementedException();
+        }
+    }
+}
