fe6a559f298719015aa59bf17f9fbd91c7e8b90c
Alessandro <alebaldissera99@gmail.com>
2023-05-20 11:18:12 +0200

    SL-377 Completata query

--- SmartLogStatistics/Repository/StatisticsRepositoryPgSql.cs
+++ SmartLogStatistics/Repository/StatisticsRepositoryPgSql.cs
@@ -11,10 +11,27 @@ namespace SmartLogStatistics.Repository {
 
         public StatisticsDto Statistics(DateTime start, DateTime end) {
 
-            var res = from l in context.Log from f in context.File.Where(x => x.id == l.file_id) select new { f.id };
-            
+            Func<Log, bool> filterByDate = (log) => {
+                DateTime logdatetime = new(log.date.Year, log.date.Month, log.date.Day, log.time.Hour, log.time.Minute, log.time.Second, log.time.Millisecond);
+                return logdatetime >= start && logdatetime <= end;
+            };
 
-            throw new NotImplementedException();
+            var filesAndEventCount = from log in context.Log.Where(filterByDate)
+                      join file in context.File
+                        on log.file_id equals file.id into logfile
+                      select new { FileId = log.file_id, EventsCount =  logfile.Count() };
+
+            int NumberOfFiles = filesAndEventCount.Count();
+            int MaxNumberOfEvent = filesAndEventCount.Max((x) => x.EventsCount);
+            int AverageNumberOfEvents = (int)filesAndEventCount.Average((x) => x.EventsCount);
+            int StandardDeviation = (int)filesAndEventCount.Average(x => x.EventsCount - AverageNumberOfEvents);
+
+            return new StatisticsDto(start, end, new List<StatisticsDto.Statistic> {
+                new StatisticsDto.Statistic("Numero di file", NumberOfFiles),
+                new StatisticsDto.Statistic("Massimo numero di eventi", MaxNumberOfEvent),
+                new StatisticsDto.Statistic("Media di eventi", AverageNumberOfEvents),
+                new StatisticsDto.Statistic("Deviazione standard per file di log", StandardDeviation)
+            });
         }
     }
 }
