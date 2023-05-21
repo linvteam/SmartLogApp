038b773be604dcf7750eb00c447ccfa9185fba99
Mauro Carnuccio <mauroboh@gmail.com>
2023-05-21 17:17:58 +0200

    SL-378 Aggiunte classi base

--- SmartLogStatistics/Model/CodeWithDescriptionDto.cs
+++ SmartLogStatistics/Model/CodeWithDescriptionDto.cs
@@ -2,5 +2,14 @@
 {
     public class CodeWithDescriptionDto
     {
+        public string Code { get; private set; }
+        public string Description { get; private set; }
+        
+        public CodeWithDescriptionDto(string code, string description) {
+            Code = code;
+            Description = description;
+        }
+
+        
     }
 }
---
+++ SmartLogStatistics/Repository/InfoRepositoryPgSql.cs
@@ -0,0 +1,37 @@
+﻿using SmartLogStatistics.Model;
+using System.Diagnostics;
+
+namespace SmartLogStatistics.Repository {
+    public class InfoRepositoryPgSql: InfoRepository {
+
+        private readonly SmartLogContext context;
+
+        public InfoRepositoryPgSql(SmartLogContext db) {
+            context = db;
+        }
+
+        /// <summary>
+        /// Comunica col database per restituire la lista di tutti i codici con le loro descrizioni
+        /// </summary>
+        /// <returns>La lista di codici con le descrizioni</returns>
+        public List<CodeWithDescriptionDto> GetCodesWithDescription() {
+
+            try {
+                return new List<CodeWithDescriptionDto>(){new("Prova", "DescrizioneProva")};
+                
+
+            } catch(Exception e) {
+                Debug.WriteLine($"ECCEZIONE {e.Message}");
+                return new List<CodeWithDescriptionDto>(){new("", "")};
+            }
+        }
+
+        public DateTimeIntervalDto GetTimeInterval() {
+            throw new NotImplementedException();
+        }
+
+        public List<string> GetFirmwareList() {
+            throw new NotImplementedException();
+        }
+    }
+}
