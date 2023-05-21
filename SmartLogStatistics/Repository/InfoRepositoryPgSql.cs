fa0cf3d62133fc120fe4b0a8c93cde9a389e13e3
Mauro Carnuccio <mauroboh@gmail.com>
2023-05-21 18:29:35 +0200

    SL-378 Implementato CodeWithDescription Aggiunto servicelifetime.singleton al context

--- SmartLogStatistics/Repository/InfoRepositoryPgSql.cs
+++ SmartLogStatistics/Repository/InfoRepositoryPgSql.cs
@@ -2,10 +2,16 @@
 using System.Diagnostics;
 
 namespace SmartLogStatistics.Repository {
+    
+    [Core.Injectables.Singleton(typeof(InfoRepository))]
     public class InfoRepositoryPgSql: InfoRepository {
 
         private readonly SmartLogContext context;
 
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="db"> Il context che si connette al database</param>
         public InfoRepositoryPgSql(SmartLogContext db) {
             context = db;
         }
@@ -16,15 +22,15 @@ namespace SmartLogStatistics.Repository {
         /// <returns>La lista di codici con le descrizioni</returns>
         public List<CodeWithDescriptionDto> GetCodesWithDescription() {
 
-            try {
-                return new List<CodeWithDescriptionDto>(){new("Prova", "DescrizioneProva")};
-                
+            
+            List<CodeWithDescriptionDto> result = this.context.Event
+                                                      .Select(l => new CodeWithDescriptionDto(l.code, l.description))
+                                                      .ToList();
 
-            } catch(Exception e) {
-                Debug.WriteLine($"ECCEZIONE {e.Message}");
-                return new List<CodeWithDescriptionDto>(){new("", "")};
-            }
+            // return new List<CodeWithDescriptionDto>(){new(e.code, e.description)};
+            return result;
         }
+
 
         public DateTimeIntervalDto GetTimeInterval() {
             throw new NotImplementedException();
