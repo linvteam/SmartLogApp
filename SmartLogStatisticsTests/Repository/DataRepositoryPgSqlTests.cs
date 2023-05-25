80a8955828067fc7394d2eea1fde449c268c0a53
Nicola Ravagnan <nicolaravagnan4@gmail.com>
2023-05-25 13:34:45 +0200

    SL-376 Creato file di test

---
+++ SmartLogStatisticsTests/Repository/DataRepositoryPgSqlTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SmartLogStatistics.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartLogStatistics.Repository.Tests {
+    [TestClass()]
+    public class DataRepositoryPgSqlTests {
+        [TestMethod()]
+        public void DataRepositoryPgSqlTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void FrequencyTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CumulativeTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void TotalByCodeTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void TotalByFirmwareTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
