22e556bb408554dd205a890e6ddd2d4c3b8f0a76
Alessandro Baldissera <alebaldissera99@gmail.com>
2023-05-26 21:36:30 +0200

    SL-377 aggiunta documentazione mancante

--- SmartLogStatistics/Exceptions/EmptyOrFailedQuery.cs
+++ SmartLogStatistics/Exceptions/EmptyOrFailedQuery.cs
@@ -1,8 +1,17 @@
 ﻿namespace SmartLogStatistics.Exceptions {
+    /// <summary>
+    /// Eccezione che rappresenta il caso di query fallita o che non produce un risultato
+    /// </summary>
     public class EmptyOrFailedQuery: Exception {
 
+        /// <summary>
+        /// Codice numerico associato all'eccezione
+        /// </summary>
         public int Code { get; private set; }
 
+        /// <summary>
+        /// Crea un nuovo oggetto impostando codice e messaggio dell'errore
+        /// </summary>
         public EmptyOrFailedQuery(): base("La query non ha prodotto risultati") {
             this.Code = 6;
         }
