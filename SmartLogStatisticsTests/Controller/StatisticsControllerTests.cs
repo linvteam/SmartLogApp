38762119b09a1039b26363249ba882e60c1d0b24
Matteo Cusin <matteo.cusin58@gmail.com>
2023-05-27 11:35:31 +0200

    SL-382 Fix test dopo merge devel

--- SmartLogStatisticsTests/Controller/StatisticsControllerTests.cs
+++ SmartLogStatisticsTests/Controller/StatisticsControllerTests.cs
@@ -15,11 +15,17 @@ namespace SmartLogStatistics.Controller.Tests
         [TestMethod()]
         public void StatisticsSuccessfulTest()
         {
-            Mock<StatisticsRepository> repository = new();
-            Mock<StatisticsDto> dto = new();
-            repository.Setup(x => x.Statistics(It.IsAny<DateTime>(), It.IsAny<DateTime>())).Returns(dto.Object);
             DateTime start = new DateTime(2023, 01, 01, 15, 0, 0, 000);
             DateTime end = new DateTime(2023, 01, 10, 15, 0, 0, 000);
+
+            Mock<StatisticsRepository> repository = new();
+            StatisticsDto dto = new StatisticsDto(start, end, new List<StatisticsDto.Statistic> {
+                new StatisticsDto.Statistic("Numero di file", 1),
+                new StatisticsDto.Statistic("Massimo numero di eventi", 1),
+                new StatisticsDto.Statistic("Media di eventi", 1),
+                new StatisticsDto.Statistic("Deviazione standard", 0)
+            });;
+            repository.Setup(x => x.Statistics(It.IsAny<DateTime>(), It.IsAny<DateTime>())).Returns(dto);
 
             StatisticsController statisticsController = new(repository.Object);
             ObjectResult result = (ObjectResult)statisticsController.Statistics(start, end);
