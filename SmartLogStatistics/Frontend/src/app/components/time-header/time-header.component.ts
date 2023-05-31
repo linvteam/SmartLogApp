0710c6474b2e934b7df6cfc30603f315fe19b979
Alessandro <alebaldissera99@gmail.com>
2023-05-31 23:12:53 +0200

    SL-409 Aggiunto fetch dati sul time-header

--- SmartLogStatistics/Frontend/src/app/components/time-header/time-header.component.ts
+++ SmartLogStatistics/Frontend/src/app/components/time-header/time-header.component.ts
@@ -1,52 +1,65 @@
+import { HttpResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
-import {FormBuilder} from "@angular/forms";
+import { FormBuilder } from "@angular/forms";
+import { InfoService } from '../../services/info/info.service';
 
 @Component({
-  selector: 'app-time-header',
-  templateUrl: './time-header.component.html',
-  styleUrls: ['./time-header.component.css']
+    selector: 'app-time-header',
+    templateUrl: './time-header.component.html',
+    styleUrls: ['./time-header.component.css']
 })
 export class TimeHeaderComponent {
-  
-  public startDatetimeValue: string = '2021-05-27T12:00';
-  public endDatetimeValue: string = '2023-05-27T12:00';
 
-  public minDate = this.startDatetimeValue;
-  public maxDate = this.endDatetimeValue;
+    public startDatetimeValue: string = '';
+    public endDatetimeValue: string = '';
+
+    public minDate = this.startDatetimeValue;
+    public maxDate = this.endDatetimeValue;
 
 
-  /**
-   * Gestore del form
-   */
-  formGroup = this.formBuilder.group({
-    startDatetime: '0',
-    endDatetime: '1'
-  });
+    /**
+     * Gestore del form
+     */
+    formGroup = this.formBuilder.group({
+        startDatetime: '0',
+        endDatetime: '1'
+    });
 
-  /**
-   * Crea una nuova istanza del controller del widget di inserimento dell'intervallo temporale
-   * @param formBuilder Servizio di gestione dei form
-   */
-  constructor(private formBuilder: FormBuilder) {
+    /**
+     * Crea una nuova istanza del controller del widget di inserimento dell'intervallo temporale
+     * @param formBuilder Servizio di gestione dei form
+     */
+    constructor(private formBuilder: FormBuilder, private infoRepository: InfoService) {
+        infoRepository.GetTimeInterval().subscribe({
+            next: (event: any) => {
+                if (event instanceof HttpResponse<any>) {
+                    this.minDate = event.body.start;
+                    this.maxDate = event.body.end;
+                    this.startDatetimeValue = this.minDate;
+                    this.endDatetimeValue = this.maxDate;
+                }
+            }, error: (err) => {
+                console.log(err);
+            }
+        });
+    }
 
-  }
+    /**
+     * Metodo che gestisce il submit del form.
+     */
+    public submitForm(): void {
 
-  /**
-   * Metodo che gestisce il submit del form.
-   */
-  public submitForm(): void {
+        const startDatetime = this.formGroup.value.startDatetime ? new Date(this.formGroup.value.startDatetime) : null;
+        const endDatetime = this.formGroup.value.endDatetime ? new Date(this.formGroup.value.endDatetime) : null;
 
-    const startDatetime = this.formGroup.value.startDatetime ? new Date(this.formGroup.value.startDatetime) : null;
-    const endDatetime = this.formGroup.value.endDatetime ? new Date(this.formGroup.value.endDatetime) : null;
+        console.log("INIZIO:");
+        console.log(startDatetime!.getTime());
+        console.log("FINE:");
+        console.log(endDatetime!.getTime());
 
-    console.log("INIZIO:");
-    console.log(startDatetime!.getTime());
-    console.log("FINE:");
-    console.log(endDatetime!.getTime());
+    }
 
-  }
-
-  public formatDate(date: string) : string {
-    return (new Date(date)).toISOString().slice(0, 16);
-  }
+    public formatDate(date: string): string {
+        return (new Date(date)).toISOString().slice(0, 16);
+    }
 }
