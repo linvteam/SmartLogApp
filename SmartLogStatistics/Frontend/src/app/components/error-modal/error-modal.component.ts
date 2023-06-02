28b504a75fe9c16b63dd4c5e78fcabbbfa1f1a7f
Riccardo014 <116018411+Riccardo014@users.noreply.github.com>
2023-06-02 23:36:42 +0200

    SL-409 aggiunti commenti a ErrorModalComponent

--- SmartLogStatistics/Frontend/src/app/components/error-modal/error-modal.component.ts
+++ SmartLogStatistics/Frontend/src/app/components/error-modal/error-modal.component.ts
@@ -1,25 +1,47 @@
 import { Component } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
+/**
+ * Classe per la gestione degli errori 
+ */
 @Component({
-  selector: 'app-error-modal',
-  templateUrl: './error-modal.component.html',
-  styleUrls: ['./error-modal.component.css']
-})
-export class ErrorModalComponent {
-    private callback: any;
-    public errorMessage: string = '';
-
-    constructor(public activeModal: NgbActiveModal) {
-    }
-
-    public setup(errorMessage: string, callback: any) {
-        this.errorMessage = errorMessage;
-        this.callback = callback;
-    }
-
-    public retry() {
-        this.activeModal.close();
-        this.callback();
-    }
-}
+    selector: 'app-error-modal',
+    templateUrl: './error-modal.component.html',
+    styleUrls: ['./error-modal.component.css']
+  })
+  export class ErrorModalComponent {
+      /**
+       * Funzione di callback
+       * @private
+       */
+      private callback: any;
+      /**
+       * Messaggio di errore
+       */
+      public errorMessage: string = '';
+  
+      /**
+       * Costruttore
+       * @param activeModal Modal attivo
+       */
+      constructor(public activeModal: NgbActiveModal) {
+      }
+  
+      /**
+       * Setup del modal di errore
+       * @param errorMessage Messaggio di errore
+       * @param callback Funzione di callback
+       */
+      public setup(errorMessage: string, callback: any) {
+          this.errorMessage = errorMessage;
+          this.callback = callback;
+      }
+  
+      /**
+       * Metodo che chiude il modal aperto e chiama la funzione di callback
+       */
+      public retry() {
+          this.activeModal.close();
+          this.callback();
+      }
+  }
