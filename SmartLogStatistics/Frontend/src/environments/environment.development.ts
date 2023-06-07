7c1a719ca73b3390d03a2cd3500209205a316ade
Alessandro <alebaldissera99@gmail.com>
2023-06-07 12:14:12 +0200

    SL-412 preparata app per il rilascio

---
+++ SmartLogStatistics/Frontend/src/environments/environment.development.ts
@@ -0,0 +1,7 @@
+/**
+ * Ambiente di default per lo sviluppo
+ */
+export const environment = {
+    production: true,
+    BaseURL: 'https://localhost:7253/api'
+};
---
+++ SmartLogStatistics/Frontend/src/environments/environment.production.ts
@@ -0,0 +1,7 @@
+/**
+ * Ambiente di default per la produzione
+ */
+export const environment = {
+    production: true,
+    BaseURL: '/api'
+};
