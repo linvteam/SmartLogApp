68edfcade1633a34793f584a913c9aaadf2dbfdd
Matteo Cusin <matteo.cusin58@gmail.com>
2023-06-11 22:56:15 +0200

    SL-413 Aggiunta test

--- SmartLogStatistics/Frontend/src/app/file-dropped.directive.spec.ts
+++ SmartLogStatistics/Frontend/src/app/file-dropped.directive.spec.ts
@@ -5,4 +5,43 @@ describe('FileDroppedDirective', () => {
     const directive = new FileDroppedDirective();
     expect(directive).toBeTruthy();
   });
+
+  it('should handle drag over empty', () => {
+    let e: any = {
+      preventDefault: () => {},
+      stopPropagation: () => {},
+      dataTransfer: {
+        files: [new File([''], 'test-file.txt')]
+      }
+    };
+
+    const directive = new FileDroppedDirective();
+    let spy_prevent = spyOn(e, 'preventDefault');
+    let spy_propagation = spyOn(e, 'stopPropagation');
+
+    directive.onDrop(e);
+
+    expect(spy_prevent).toHaveBeenCalled();
+    expect(spy_propagation).toHaveBeenCalled();
+  });
+
+  it('should handle drag over with values', () => {
+    let e: any = {
+      preventDefault: () => {},
+      stopPropagation: () => {},
+      dataTransfer: {
+        files: [new File(['a,v'], 'test-file.txt')]
+      }
+    };
+
+    const directive = new FileDroppedDirective();
+    let spy_prevent = spyOn(e, 'preventDefault');
+    let spy_propagation = spyOn(e, 'stopPropagation');
+
+    directive.onDrop(e);
+
+    expect(spy_prevent).toHaveBeenCalled();
+    expect(spy_propagation).toHaveBeenCalled();
+  });
+
 });
